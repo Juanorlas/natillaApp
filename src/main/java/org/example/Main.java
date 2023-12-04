8cc0f6278c1eb5e4af7929c33f636c28d923d157
jablancohe@cesde.net <jablancohe@cesde.net>
2023-12-04 15:35:07 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Entrada del Problema
+        String nombreUsuario="Juan Diego Orlas Rengifo";
+        short edadUsuario=32;
+        String telefonoUsuario="35463541321635";
+        String documentoIdentidad="1035858511";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direccionUsuario="Nuevo Horizonte";
+        double costoInscripcion=150000.0;
+        float tasaInteresNatillera=0.1F;
+        double saldoFinalUsuario;
+
+        //PROCESO
+
+        //1. Asignar un valor especifico a una variable desde el teclado (recibir datos)
+
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("Hola soy una IA, cual es tu nombre?");
+        leerDato.nextLine();
+
+
+
+
+    }
+}
